a6f551aff9cad5487c1d02be5c45720420ad1b5d
Arjun Adhia <arjun.adhia@gmail.com>
2020-03-27 17:12:25 -0700

    Added Logging

--- myBot.py
+++ myBot.py
@@ -3,8 +3,15 @@
 from discord.ext import commands
 import discord
 import random
+import logging
 
 from Game import Game
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 bot = commands.Bot(command_prefix='$')
 
